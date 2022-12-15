ca242821b17ea55d64a12e25dde4bfdd9fb285bf
F-Almeman <77334154+F-Almeman@users.noreply.github.com>
2022-12-15 21:37:00 +0000

    Update wsi_clustering.py

--- wsi/wsi_clustering.py
+++ wsi/wsi_clustering.py
@@ -38,7 +38,7 @@ def cluster_inst_ids_representatives(inst_ids_to_representatives: Dict[str, List
         embed = np.concatenate((def_vec, vec_1)) # Its shape is (997,)
         print(embed.shape)
         print(type(embed))
-        np.append(new_embed, embed)
+        new_embed = np.append(new_embed, embed)
       print(new_embed.shape)
       print(type(new_embed))
       return new_embed
@@ -69,7 +69,7 @@ def cluster_inst_ids_representatives(inst_ids_to_representatives: Dict[str, List
         combined_embed = combine(transformed[i * n_represent:(i + 1) * n_represent], definitions_embeddings[i])
         print(combined_embed.shape)
         print(type(combined_embed))
-        np.append(combined_embeddings, combined_embed)
+        combined_embeddings = np.append(combined_embeddings, combined_embed)
     
     print(type(combined_embeddings))
     print(combined_embeddings.shape)
