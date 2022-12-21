a7ca75a62370be8a196850de5b38320a50d2cd91
F-Almeman <77334154+F-Almeman@users.noreply.github.com>
2022-12-21 15:19:46 +0000

    Update wsi_clustering.py

--- wsi/wsi_clustering.py
+++ wsi/wsi_clustering.py
@@ -45,7 +45,7 @@ def cluster_inst_ids_representatives(inst_ids_to_representatives: Dict[str, List
         return new_embed
       if pca == True:
         pca = PCA(n_components=500)
-        return [pca.fit_transform(emb) for emb in new_embed]
+        return [pca.fit_transform(emb.reshape(-1, 1)) for emb in new_embed]
 
     inst_ids_ordered = list(inst_ids_to_representatives.keys())
     lemma = inst_ids_ordered[0].rsplit('.', 1)[0]
