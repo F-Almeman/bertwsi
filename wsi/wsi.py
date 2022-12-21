8fdc75693f0af3542ebb26760c42ad54bd306eab
F-Almeman <77334154+F-Almeman@users.noreply.github.com>
2022-12-21 13:40:49 +0000

    Update wsi.py

--- wsi/wsi.py
+++ wsi/wsi.py
@@ -42,8 +42,7 @@ class WordSenseInductor:
                 if row['WORD'] == lemma_pos:
                     inst_id_to_definition = {row['WORD_ID'][i]: row['DEFINITION'][i] for i in range(len(row['WORD_ID']))}
                     break
-            
-            '''
+
             print("**************"+"\n"+lemma_pos+"\n")
             for ids in inst_id_to_sentence:
                 print("\n"+ids)
@@ -51,10 +50,7 @@ class WordSenseInductor:
                 print(inst_id_to_definition[ids])
                 for i in inst_ids_to_representatives[ids]:
                     print(i)
-                
-            '''
           
-            
             clusters, statistics = cluster_inst_ids_representatives(
                 inst_ids_to_representatives=inst_ids_to_representatives, inst_id_to_definition=inst_id_to_definition,
                 max_number_senses=wsisettings.max_number_senses,min_sense_instances=wsisettings.min_sense_instances,
