a9d809d6026d6c68628c8b82f5c4213a2969c35e
F-Almeman <77334154+F-Almeman@users.noreply.github.com>
2023-01-02 16:37:20 +0000

    Update wsi_clustering.py

--- wsi/wsi_clustering.py
+++ wsi/wsi_clustering.py
@@ -76,7 +76,7 @@ def cluster_inst_ids_representatives(inst_ids_to_representatives: Dict[str, List
     combined_embeddings = []
     for i, inst_id in enumerate(inst_ids_ordered):
         # combine representatives' vectors "<class 'numpy.matrix'>" and definitions' embeddings "<class 'numpy.ndarray'>"
-        combined_embed = combine(transformed[i * n_represent:(i + 1) * n_represent], definitions_embeddings[i])
+        combined_embed = combine_2(transformed[i * n_represent:(i + 1) * n_represent], definitions_embeddings[i])
         combined_embeddings.append(combined_embed)
     
     combined_embeddings = [y for x in combined_embeddings for y in x]
