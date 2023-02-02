fa6ae1851ddc116f5182afcfcb93fac4687a6118
F-Almeman <77334154+F-Almeman@users.noreply.github.com>
2023-02-02 20:03:16 +0000

    Update lm_bert.py

--- wsi/lm_bert.py
+++ wsi/lm_bert.py
@@ -255,7 +255,7 @@ class LMBert(SLM):
 
                 #logits_all_tokens = self.bert(torch_input_ids, attention_mask=torch_mask)
                 #logits_all_tokens = self.bert.embeddings(torch_input_ids, attention_mask=torch_mask)
-                logits_all_tokens = self.embeddings(torch_input_ids.cuda())
+                logits_all_tokens = self.embeddings(torch_input_ids.to(device=self.device))
                 
                 print("\n\nSize of logits_all_tokens : "+str(len(logits_all_tokens)))
                 print(type(logits_all_tokens))
