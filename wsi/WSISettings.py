eae1c493997707d7865391799d8999fd7cd5165e
F-Almeman <77334154+F-Almeman@users.noreply.github.com>
2023-02-11 11:15:22 +0000

    Update WSISettings.py

--- wsi/WSISettings.py
+++ wsi/WSISettings.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
-WSISettings = namedtuple('WSISettings', ['n_represents', 'n_samples_per_rep', 'cuda_device', 'debug_dir',
+WSISettings = namedtuple('WSISettings', ['dm_model','n_represents', 'n_samples_per_rep', 'cuda_device', 'debug_dir',
                                          'disable_tfidf', 'disable_lemmatization', 'run_name', 'patterns',
                                          'min_sense_instances', 'bert_model',
                                          'max_batch_size', 'prediction_cutoff', 'max_number_senses',
