74eb736716f9b8d90434aa1d903f6e16a6dd6ae1
F-Almeman <77334154+F-Almeman@users.noreply.github.com>
2023-03-16 13:22:32 +0000

    Update wsi_clustering.py

--- wsi/wsi_clustering.py
+++ wsi/wsi_clustering.py
@@ -35,7 +35,7 @@ def cluster_inst_ids_representatives(inst_ids_to_representatives: Dict[str, List
       new_embed = []
       for vec in rep_vec:
         vec_1 = vec.A1    # to convert from matrix to array
-        3embed = np.concatenate((def_vec, vec_1))
+        #embed = np.concatenate((def_vec, vec_1))
         #embed = np.kron(def_vec, vec_1)
         embed = np.multiply.outer(def_vec, vec_1)
         new_embed.append(embed)
