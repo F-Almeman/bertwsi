d72ed54ba9adca0a948465a94af547f32ecd13d2
F-Almeman <77334154+F-Almeman@users.noreply.github.com>
2023-04-14 00:27:59 +0100

    Update wsi_clustering.py

--- wsi/wsi_clustering.py
+++ wsi/wsi_clustering.py
@@ -38,7 +38,7 @@ def cluster_inst_ids_representatives(inst_ids_to_representatives: Dict[str, List
         vec_1 = vec_1[vec_1 != 0] # to extract the values not equal to zero
         vec_1 = (vec_1-np.min(vec_1))/(np.max(vec_1)-np.min(vec_1)) # min max normalization
         embed = np.concatenate((def_vec, vec_1))
-        new_embed.append(embed_1)
+        new_embed.append(embed)
 
       return new_embed
     
